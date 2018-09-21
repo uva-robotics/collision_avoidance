f5c61e2db97e652083d1fdb138138e738e9d7e77
Chris Al Gerges <chrisalgerges@hotmail.com>
2018-09-21 12:54:10 +0200

    trying to get the lasers working

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ emergency_stop = False
 RANGE = 0.75
 
 def speech():
-    publisher_speech.publish("Pardon me, could you please move aside?")
+    publisher_speech.publish("test")
 
 def sonar_front(data):
     global emergency_stop
@@ -29,9 +29,11 @@ def sonar_front(data):
         publisher_velocity.publish(twist_msg)
         speech()
         emergency_stop = True
+        time.sleep(4)
+        emergency_stop = False
 
 def sonar_back(data):
-    print(data.range)
+    global emergency_stop
     if data.range < RANGE and emergency_stop is False:
         twist_msg = Twist()
         twist_msg.linear.x = 0
@@ -44,11 +46,13 @@ def sonar_back(data):
         print("EMERGENCY STOP BACK", data.range)
         publisher_velocity.publish(twist_msg)
         emergency_stop = True
+        time.sleep(4)
+        emergency_stop = False
 
 
-# def laser_detector(data):
-#     print(data.ranges)
-#     time.sleep(1)
+def laser_detector(data):
+    ranges = [x for x in data.ranges if x != -1.0]
+    print(ranges[30:44])
 
 
 if __name__ == '__main__':
@@ -56,6 +60,6 @@ if __name__ == '__main__':
     publisher_speech = rospy.Publisher("/speech", String, queue_size=10)
     publisher_velocity = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     # rospy.Subscriber("/pepper_robot/sonar/back", Range, sonar_back)
-    rospy.Subscriber("/pepper_robot/sonar/front", Range, sonar_front)
-    # rospy.Subscriber("/pepper_robot/laser", LaserScan, laser_detector)
+    # rospy.Subscriber("/pepper_robot/sonar/nt", Range, sonar_front)
+    rospy.Subscriber("/pepper_robot/laser", LaserScan, laser_detector)
     rospy.spin()
